cc65ab8b312baf89390715d5a296f38a9acc1f5b
kishan <kishan.excel2011@gmail.com>
2023-11-03 13:04:24 +0530

    netid

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import {
   useDisclosure,
   ColorModeProvider,
   CSSReset,
-  Alert,
-  AlertIcon,
 } from '@chakra-ui/react';
 import themes from './Theme/Theme';
 import ReactGA from 'react-ga';
@@ -25,7 +23,6 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/common/Navbar';
 import Sidebar from './Components/common/Sidebar';
 import DashboardHeader from './Components/common/DashboardHeader';
-import { NeworkID } from './config';
 
 function App(props) {
   const [isDataLoad, setIsDataLoad] = useState(false);
@@ -84,16 +81,6 @@ function App(props) {
                 </GridItem>
                 <GridItem className='example' sx={{ flexGrow: 1, overflow: "auto", }}>
                   <Box px={2}>
-                    <Box pb={2}>
-                      {
-                        props.MobXStorage.netId && NeworkID !== props.MobXStorage.netId
-                          ?
-                          (
-                            <Alert status='error' sx={{ color: "red", fontSize: "sm", display: "flex", justifyContent: "center", alignItems: "center" }}>  <AlertIcon color="red" />ERROR: WRONG NETWORK</Alert>
-                          )
-                          : null
-                      }
-                    </Box>
                     <DashboardHeader />
                   </Box>
                   <Switch>
